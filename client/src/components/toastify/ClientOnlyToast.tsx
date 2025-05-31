5b029742ee960bed57c7ced37587e08890005ea7
PhuongHE173077 <phuongddhe173077@fpt.edu.vn>
2025-05-31 15:08:25 +0700

    Fix SSR in root layout

--- client/src/components/toastify/ClientOnlyToast.tsx
+++ client/src/components/toastify/ClientOnlyToast.tsx
@@ -1,9 +1,16 @@
 // app/components/ClientOnlyToast.tsx
 'use client';
-
+import { useEffect, useState } from "react";
 import { ToastContainer } from 'react-toastify';
 
 export default function ClientOnlyToast() {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) return null;
     return (
         <ToastContainer
             position="top-right"
